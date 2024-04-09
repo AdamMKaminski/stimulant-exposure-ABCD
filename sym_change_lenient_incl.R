e6697ddebe35087cfd08dad24ae6d479525e18d7
Adam M Kaminski <adam.kaminski@tufts.edu>
2024-04-09 17:56:25 -0400

    added inclusion steps to obtain additional IDs for medication decoding

--- sym_change_lenient_incl.R
+++ sym_change_lenient_incl.R
@@ -12,6 +12,32 @@ ksads <- ksads[ksads$adhd_present.0==1,]
 ksads <- ksads[complete.cases(ksads), ]
 
 baseline_adhd_IDs <- ksads$ID
+writeLines(paste(dim(ksads)[1],"/",origlen," (", (dim(ksads)[1]/origlen)*100,"%) of people have ADHD at baseline according to KSADS",sep=""))
 
-writeLines(paste(dim(ksads)[1],"/",origlen," (", round(origlen/dim(ksads)[1]),"%) of people have ADHD at baseline according to KSADS",sep=""))
+# Get CBCL data -- is there CBCL data for these subjects at baseline and Y2? Exclude if not
+setwd("/Users/adamkaminski/Desktop/stimproj/data/ABCD_data/")
+cbcl <- read.csv('abcd_cbcls01.csv')
+cbcl <- cbcl[cbcl$A.ID%in%baseline_adhd_IDs,]
+
+cbcl_baseline <- cbcl[cbcl$time=="0_baseline_year_1_arm_1",]
+cbcl_1 <- cbcl[cbcl$time=="1_year_follow_up_y_arm_1",]
+cbcl_2 <- cbcl[cbcl$time=="2_year_follow_up_y_arm_1",]
+
+full_IDs <- cbcl_baseline$A.ID[cbcl_baseline$A.ID%in%cbcl_2$A.ID]
+writeLines(paste(length(full_IDs),"/",dim(cbcl_baseline)[1]," (", (length(full_IDs)/dim(cbcl_baseline)[1])*100,"%) of people who have CBCL baseline data also have CBCL Y2 data",sep=""))
+
+# Exclude subjects whose med data was previously decoded
+setwd("/Users/adamkaminski/Desktop/stimproj/data/")
+orig_dat <- read.csv('stimulant_master_df.csv')
+orig_IDs <- orig_dat$ID
+
+full_IDs_new <- full_IDs[!(full_IDs%in%orig_IDs)]
+
+# Get Medication data, save csv to be decoded
+setwd("/Users/adamkaminski/Desktop/stimproj/data/")
+med <- read.csv('medsy01.csv')
+
+med_subs <- med[med$The.NDAR.Global.Unique.Identifier..GUID..for.research.subject%in%full_IDs_new,]
+write.csv(med_subs,"medsy01_moreIDs.csv",na = "",row.names = FALSE)
+
 
